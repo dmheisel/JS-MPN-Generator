d38b1a5ff3b033d1eb76c3c0d887f835e25cb5cb
David Heisel <d.m.heisel@gmail.com>
2019-06-05 16:50:54 -0500

    added script, table import

---
+++ script.js
@@ -0,0 +1,14 @@
+const table = require('./MPNtable.json');
+const data = table.data;
+
+const mpnIndex = table.fields.indexOf('MPN Index/100mL');
+const combinationIndex = table.fields.indexOf('Combination of Positives');
+const lowIndex = table.fields.indexOf("Confidence Limit LOW");
+const highIndex = table.fields.indexOf("Confidence Limit HIGH");
+
+const mpnFinder = (input) => {
+    const mpn = data.find(x => x[combinationIndex] === input)[mpnIndex];
+    if (mpn === undefined) {
+        return "ERROR - COMBINATION PROVIDED IS NOT ON TABLE";
+    } else return mpn;
+};
