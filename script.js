bc301cf72305d04a977cd3b05236d50103c6f5f1
dmheisel <d.m.heisel@gmail.com>
2019-06-22 21:43:14 -0500

    Updated MPNtable to array in js file instead of json object, removing need for browserify bundle.  script updated to correspond to new mpntable array.  html file updated to add both script files in git status

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-const table = require('./MPNtable.json');
-const data = table.data;
-const mpnIndex = table.fields.indexOf('MPN Index/100mL');
-const combinationIndex = table.fields.indexOf('Combination of Positives');
-const lowIndex = table.fields.indexOf("Confidence Limit LOW");
-const highIndex = table.fields.indexOf("Confidence Limit HIGH");
-
+const data = mpnTable;
+const combinationIndex = 0;
+const mpnIndex = 1;
+const lowIndex = 2;
+const highIndex = 3;
 
 const submitButton = document.getElementById('submit-button');
 
 const mpnFinder = (input) => {
-    let mpn
-    for (i=0; i < data.length; i++){
-      let item = data[i];
-      for (j=0; j < item.length; j++) {
-        //console.log(item);
-        if (item[combinationIndex] === input) {
-          console.log(item);
-          let mpn = item[mpnIndex];
-          return mpn;
-        }
-      }
-    }
-    if (mpn === undefined) {
-        return "ERROR - COMBINATION PROVIDED IS NOT ON TABLE";
-    } else return mpn;
+  let mpn;
+  for (i = 0; i < data.length; i++) {
+    if (data[i][combinationIndex] === input) {
+      mpn = data[i][mpnIndex];
+    };
+  }
+  if (mpn === undefined) {
+    console.log("ERROR - Combination provided not on table.")
+  } else {
+    return mpn;
+  };
 };
 
-
-submitButton.onclick = function() {
+submitButton.onclick = () => {
 
   const first = document.getElementById('result-one').value;
   const second = document.getElementById('result-two').value;
